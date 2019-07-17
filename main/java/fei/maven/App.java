85aa9e9e291408f4ae078cf1ee76f386a8fc645b
feixia5320 <1044489176@qq.com>
2019-07-17 22:38:26 +0800

    GetMapping

--- main/java/fei/maven/App.java
+++ main/java/fei/maven/App.java
@@ -1,7 +1,9 @@
 package fei.maven;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Hello world!
@@ -13,5 +15,8 @@ public class App
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
+//        SpringApplicationBuilder builder = new SpringApplicationBuilder(App.class);
+//        builder.bannerMode(Banner.Mode.OFF).run(args);
+        
     }
 }
--- main/java/fei/maven/helloWorldController.java
+++ main/java/fei/maven/helloWorldController.java
@@ -1,5 +1,6 @@
 package fei.maven;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,11 @@ public class helloWorldController {
 	@RequestMapping("/hello")
 	public String say(){
         return "Hello Spring Boot";
+    }
+	
+	@GetMapping("/hello2")
+	public String say2(){
+        return "Hello Spring Boot2";
     }
 
 }
